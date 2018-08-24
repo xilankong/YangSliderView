9e8440bf5efd76ea0191636e15a6efd46f23292b
young.huang <young.huang@jfz.com>
2018-08-24 17:14:26 +0800

    addTag : 1.0.1

--- Example/YangSliderView/ViewController.swift
+++ Example/YangSliderView/ViewController.swift
@@ -7,17 +7,45 @@
 //
 
 import UIKit
-
+import SnapKit
+import YangSliderView
 class ViewController: UIViewController {
 
+    
+    var vcs = [UIViewController & YangSliderViewContainerDelegate]()
+    var titles = [String]()
+    let slideMenu = YangSliderView()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.white
+        
+        let vc1 = YangSliderViewContainerViewController()
+        vc1.view.backgroundColor = UIColor.green
+        let vc2 = YangSliderViewContainerViewController()
+        vc2.view.backgroundColor = UIColor.yellow
+        let vc3 = YangSliderViewContainerViewController()
+        vc3.view.backgroundColor = UIColor.black
+        let vc4 = YangSliderViewContainerViewController()
+        vc4.view.backgroundColor = UIColor.purple
+        let vc5 = YangSliderViewContainerViewController()
+        vc5.view.backgroundColor = UIColor.gray
+        vcs = [vc1, vc2, vc3, vc4, vc5]
+        
+        titles = ["tab-1", "tab-2", "tab-3", "tab-4", "tab-5"]
+        
+        slideMenu.reloadView(titles: titles, controllers: vcs)
+        view.addSubview(slideMenu)
+        automaticallyAdjustsScrollViewInsets = false
+        slideMenu.snp.makeConstraints {
+            $0.left.right.bottom.equalTo(self.view)
+            $0.top.equalTo(self.view).offset(64)
+        }
+ 
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func change() {
+        let index = (slideMenu.currentIndex + 1) > titles.count - 1 ? 0 : (slideMenu.currentIndex + 1)
+        slideMenu.currentIndex = index
     }
 
 }
---
+++ Example/YangSliderView/YangSliderViewContainerViewController.swift
@@ -0,0 +1,22 @@
+//
+//  YangSliderViewContainerViewController.swift
+//  YangSliderView_Example
+//
+//  Created by yanghuang on 2018/8/24.
+//  Copyright © 2018年 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import YangSliderView
+
+class YangSliderViewContainerViewController: UIViewController, YangSliderViewContainerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func reloadView() {
+        
+    }
+    
+}
