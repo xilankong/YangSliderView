5f57266728d820ecab5629b4673a69042ac7719a
young.huang <young.huang@jfz.com>
2018-08-24 14:57:35 +0800

    Initial commit

---
+++ Example/YangSliderView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  YangSliderView
+//
+//  Created by xilankong on 08/24/2018.
+//  Copyright (c) 2018 xilankong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
